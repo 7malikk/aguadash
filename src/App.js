9444a9870b0b5c181e6730adabfadad976c8bd03
7malikk <7malikk@gmail.com>
2023-03-22 23:29:26 +0100

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import { Home } from './pages';
-import SignUp from './pages';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import {
+  Dashboard,
+  Home,
+  Login,
+  SignUp,
+  Merchant,
+  MerchantLogin,
+} from './pages';
 
 function App() {
-  return(
-    <div>
-    </div>
+  return (
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route path="nested" element={<SignUp />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/merchant" element={<Merchant />} />
+      <Route path="/merchantlogin" element={<MerchantLogin />} />
+      <Route
+        path="*"
+        element={
+          <div>
+            {' '}
+            <h1>404 page not found </h1>
+            <Link to="/">back home</Link>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
