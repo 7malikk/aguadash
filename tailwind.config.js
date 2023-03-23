1a0e59d66776a5776cb2478b227715fc2e8f0304
7malikk <7malikk@gmail.com>
2023-03-24 00:32:47 +0100

    homeUI implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    './src/**/*.{js,jsx,ts,tsx}',
     // "./public/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#026583',
+        hover: 'rgba(1, 78, 101, 1)',
+        lightAsh: '#F7F9F9',
+        skyBlue: '#E6F0F3',
+      },
+      fontFamily: {
+        play: ['Playfair Display', 'serif'],
+        lato: ['Lato', 'serif'],
+      },
+      backgroundImage: {
+        'hero-bg': "url('/src/assets/Hero.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
