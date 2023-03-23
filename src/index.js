ae22afa4fe86a0a4d562cd19d0b53b69ff85191f
Xcode-X <victorjames40v@gmail.com>
2023-03-23 10:16:46 +0100

    adding some changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <App/>
   </BrowserRouter>
 );
 
