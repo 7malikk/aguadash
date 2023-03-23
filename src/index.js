e8b13dbe8b533479ed820ceddec26951f575f4ab
Xcode-X <victorjames40v@gmail.com>
2023-03-23 09:21:01 +0100

    adding some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SignUp } from './pages';
+// import { SignUp } from './pages';
+import { MerchantLogin } from './pages';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <SignUp></SignUp>
+   <MerchantLogin></MerchantLogin>
   </React.StrictMode>
 );
 
--- src/pages/MerchantLogin.jsx
+++ src/pages/MerchantLogin.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
 const MerchantLogin = () => {
-  return <div>MerchantLogin</div>;
+  return(
+    <div>
+       <div className='pl-5'>
+      <h2>Login</h2>
+      <div className=' flex'>
+         
+          <div className='bg-blue-600 flex flex-col p-4 w-4/12 p-5'>
+            <h2>AD</h2>
+            <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quis ut, commodi est ullam dicta soluta optio obcaecati, perferendis voluptate eius corrupti nostrum odit asperiores aspernatur necessitatibus! Amet, officiis incidunt.</p>
+          </div>
+          
+          <div className='bg-blue-100 flex flex-col  w-4/12 p-5 pl-7'>
+            <h2>Merchant Login</h2>
+            <label>
+              Username
+             <br/><input type="text" value={"Enter your username"}/>
+            </label>
+            <label>
+             Enter Password
+              <br/><input type="text" value={"Enter your Password"}/>
+            </label>
+            <button>
+              Login
+            </button>
+          </div>
+      </div>
+    </div>
+    </div>
+  )
 };
 
 export default MerchantLogin;
