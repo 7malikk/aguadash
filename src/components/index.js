4d3e1e762c40205013f259211484f66b98e4043e
7malikk <7malikk@gmail.com>
2023-04-06 03:15:47 +0100

    fixes to pages and components imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-// import Sidebar from './Sidebar';
-// import Overview from './Overview';
-// import DashboardNav from './DashboardNav';
-// import History from './History';
-// import Settings from './Settings';
-// import Order from './order/Order';
+import Sidebar from './Sidebar';
+import Overview from './Overview';
+import DashboardNav from './DashboardNav';
+import History from './History';
+import Settings from './Settings';
+import Order from './order/Order';
 
-// export { Sidebar, Overview, DashboardNav, History, Settings, Order };
+export { Sidebar, Overview, DashboardNav, History, Settings, Order };
