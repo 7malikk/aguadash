abdfb3ea810324c1a3828acfb7801d9f1ac11842
7malikk <7malikk@gmail.com>
2023-04-06 03:07:35 +0100

    implementing tests

---
+++ cypress/e2e/order_spec.cy.js
@@ -0,0 +1,12 @@
+describe('order', () => {
+  it('user can place order', () => {
+    // cy.visit('/')
+    // login
+    // click on orders
+    // fill in order form and click order button
+    //click card
+    //click success
+    // click pay or go
+    //click ongoing orders
+  });
+});
